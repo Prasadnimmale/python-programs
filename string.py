ff795bc79f3eabeae4210b6f80c72c5cf42f5caa
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-15 19:49:21 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,42 @@
+# String methods helps us change,search, or format text easily
+
+
+#upper()
+#lower()
+#strip()
+#replace()
+#split()
+#count()
+#find()
+
+
+#upper()
+
+#name = "pesi"
+#print(name.upper())
+
+#upper() input format
+#text = input("enter your name:").upper()
+#print(text)
+
+#strip()
+#text = input("enter your name:").strip()
+#print(text)
+
+
+#replace
+
+#text = "Durga prasad"
+#print(text.replace("prasad","pesi"))
+
+#split()
+#text = "Durga prasad"
+#print(text.split())
+
+#count()
+#text = "Durga prasad"
+#print(text.count("s"))
+
+# find()
+text = "Durga prasad"
+print(text.find("d"))
