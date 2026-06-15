4189b7f6f3b2392fd1011ecadf5157e304314cdf
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-15 19:46:11 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,4 @@
+num = 5
+
+for i in range(1, 13):
+    print(f"{num} x {i} = {num*i}")
---
+++ swapThreeNum.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+c = 30
+
+a, b, c = b, c, a
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
